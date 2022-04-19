7b0bfbd9d9d8d9ad11d8c6c83a63e2fdf12b7bf1
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-04-20 00:32:04 +0200

    Update checker.ps1

--- checker.ps1
+++ checker.ps1
@@ -1,1 +1,8 @@
-#test
+#Check difference
+    $new = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/print_project/main/checker.ps1").content
+    $old = get-content "$($env:ProgramData)\Admin\run.ps1"
+    if(!($old -cmatch $old)){
+    
+    $new | -OutFile "$($env:ProgramData)\Admin\run.ps1" -UseBasicParsing; powershell -ep bypass "$($env:ProgramData)\Admin\run.ps1"
+    
+    }
