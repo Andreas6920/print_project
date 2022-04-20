e605591d55d9469c3cd83dd7dc6a908595c79184
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-04-20 15:07:31 +0200

    Update checker.ps1

--- checker.ps1
+++ checker.ps1
@@ -1,5 +1,5 @@
 #Check difference
-    $new = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/print_project/main/checker.ps1").content
+    $new = (Invoke-WebRequest -uri "https://raw.githubusercontent.com/Andreas6920/print_project/main/res/run.ps1").content
     $old = get-content "$($env:ProgramData)\Admin\run.ps1"
     if(!($old -cmatch $old)){
     
