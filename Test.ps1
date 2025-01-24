cba5c959e033da8f537ea6bbe41b37bf8071723a
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2025-01-24 14:30:47 +0100

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,1 @@
-irm "https://raw.githubusercontent.com/Andreas6920/print_project/refs/heads/main/print-module.psm1"  | IEX; Install-Printer -Alle -NavisionPrinter
+irm "https://raw.githubusercontent.com/Andreas6920/print_project/refs/heads/main/print-module.psm1"  | iex; Install-Printer -Alle -NavisionPrinter
